e398b643274a02966b0d5830e80d600dd52b6157
Daniel S Goncalves <dsg1407@gmail.com>
2023-03-01 22:33:35 -0300

    app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
+import { PlusCircle, ClipboardText } from "phosphor-react"
+import Logo from "./assets/todo-logo.svg"
+
 import "./styles/global.css"
 
 export function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <header className="header">
+        <img src={Logo} alt="Logo do site" />
+      </header>
+      <main>
+        <form>
+          <input type="text" placeholder="Adicione uma nova tarefa" required />
+          <button type="submit">
+            Criar
+            <PlusCircle />
+          </button>
+        </form>
+        <section className="tasks-section">
+          <section className="tasks-status-section">
+            <div>
+              <p>Tarefas criadas</p>
+              <span>0</span>
+            </div>
+            <div>
+              <p>Concluídas</p>
+              <span>0</span>
+            </div>
+          </section>
+          <section className="tasks-list-section">
+            <div>
+              <ClipboardText />
+              <strong>Você ainda não tem tarefas cadastradas</strong>
+              <p>Crie tarefas e organize seus itens a fazer</p>
+            </div>
+          </section>
+        </section>
+      </main>
     </>
   )
 }
